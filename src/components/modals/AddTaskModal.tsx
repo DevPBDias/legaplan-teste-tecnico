a96ed0dc38a6ee091552faec597f1459900e6e47
DevPBDias <devpbdias@gmail.com>
2024-09-24 16:59:33 -0300

    feat: created validations to add tasks

--- src/components/modals/AddTaskModal.tsx
+++ src/components/modals/AddTaskModal.tsx
@@ -3,21 +3,44 @@ import { useModalContext } from "@/context/providers/modal-provider";
 import "./styles.scss";
 import { useState } from "react";
 import { useTaskContext } from "@/context/providers/task-provider";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const modalSchema = z.object({
+  task: z.string().min(3, "Tarefa curta! Por favor detalhar tarefa"),
+});
+
+type ModaltData = z.infer<typeof modalSchema>;
 
 const AddTaskModal = () => {
   const { callAddModal, setCallAddModal } = useModalContext();
   const { addNewTasks, addStorage } = useTaskContext();
-  const [task, setTask] = useState<string>("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    getValues,
+  } = useForm<ModaltData>({
+    resolver: zodResolver(modalSchema),
+  });
 
-  const handleClick = () => {
-    addNewTasks(task);
-    addStorage("tasks", task);
+  const resetForm = () => {
+    if (isSubmitSuccessful) reset({ task: "" });
+  };
+
+  const handleClick = (data: ModaltData) => {
+    console.log(data);
+    addNewTasks(getValues("task"));
+    addStorage("tasks", getValues("task"));
+    resetForm();
     setCallAddModal(!callAddModal);
   };
 
   return (
     <main className="modal-container">
-      <section className="modal-content">
+      <form onSubmit={handleSubmit(handleClick)} className="modal-content">
         <h3 className="modal-title">Nova tarefa</h3>
         <fieldset>
           <label className="label-modal" htmlFor="title">
@@ -26,22 +49,23 @@ const AddTaskModal = () => {
           <input
             className="input-modal"
             type="text"
-            name="task"
-            value={task}
-            onChange={({ target }) => setTask(target.value)}
+            {...register("task")}
             id="title"
             placeholder="Digite"
           />
+          {errors.task && (
+            <span className="error-msg">{errors.task.message}</span>
+          )}
         </fieldset>
         <div className="btns-container">
           <button type="button" onClick={() => setCallAddModal(!callAddModal)}>
             Cancelar
           </button>
-          <button className="add-btn" type="button" onClick={handleClick}>
+          <button className="add-btn" type="submit">
             Adicionar
           </button>
         </div>
-      </section>
+      </form>
     </main>
   );
 };
