a6a6ed83ddf64c3827b36f164303c25fb52e0b4c
DevPBDias <devpbdias@gmail.com>
2024-09-25 23:36:52 -0300

    feat: created logic to get local date

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,21 @@ import Image from "next/image";
 import logo from "@/assets/images/logo_FocalPoint.png";
 import "./styles.scss";
 import { useUserContext } from "@/context/providers/user-provider";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { userName } = useUserContext();
+  const [date, setDate] = useState<string>("Dia, número do mês do ano");
+
+  useEffect(() => {
+    const getLocaleDate = () => {
+      let options = { dateStyle: "full" };
+
+      const todayDate = new Date().toLocaleDateString("pt-br", options as any);
+      setDate(todayDate);
+    };
+    getLocaleDate();
+  }, []);
 
   return (
     <header>
@@ -14,7 +26,7 @@ const Header = () => {
           <Image src={logo} alt="Logo FocalPoint" priority />
         </picture>
         <h1>Bem-vindo de volta, {userName === "" ? "Visitante" : userName}</h1>
-        <h4>Segunda, 01 de dezembro de 2025</h4>
+        <h4>{date.charAt(0).toUpperCase() + date.slice(1)}</h4>
       </nav>
     </header>
   );
